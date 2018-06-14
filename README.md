a01b5bfc635f4238ef57114a75333ae646f811dc
sirganguly <sirgangulytg@gmail.com>
2018-06-14 16:44:01 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+### What's this about?
+
+* Bunch of leetcode questions I scraped from some website.
+* The questions have been asked in Big 20 MNC and startups.
+* There are 3 types of questions. Easy, Medium & Hard
+* No solutions. Just the problem number and name. You can find the problems on leetcode.
+* Solutions are provided on leetcode itself Under 'Discuss' and 'Solution' tabs
+
+
+### Environment
+
+* Works on Python 2.x.y
+* For python 3.x.y, format the `print` statements yourself
+
+### How to use?
+
+* Fork repo
+* There are 2 methods you need to call:
+	* `getQuestionOfType(qType)`
+	* `getRandomQuestionOfType(qType)`
+* Replace `qType` with either `"Easy"`, `"Medium"` or `"Hard"`
---
+++ lcode.py
@@ -0,0 +1,72 @@
+﻿# -*- coding: utf-8 -*-
+from questions import get_problems
+import random
+
+data = get_problems()
+data = data.split("\n")
+
+problems = {}
+descriptions = {}
+easy, medium, hard = [], [], []
+
+def pre_process_questions():
+
+	for s in data:
+		s = s.split(" ")
+	  	
+	  	problem_num = s[0]
+	  	difficulty = s[-1]
+	  	problem_name = " ".join(s[1:len(s)-2])
+	  	
+	  	#capture problem frequency
+		if problem_num in problems:
+			problems[problem_num] += 1
+		else:
+			problems[problem_num] = 1
+	  	
+	  	
+	  	if problem_num not in descriptions:
+			descriptions[problem_num] = (problem_name, difficulty)
+
+			if difficulty == "Easy":
+				easy.append(problem_num)
+			elif difficulty == "Medium":
+				medium.append(problem_num)
+			else:
+				hard.append(problem_num)
+
+def getIterableOfType(qType):
+	iter_able = None
+	if qType == "Easy":
+		iter_able = easy
+	elif qType == "Medium":
+		iter_able = medium
+	else:
+		iter_able = hard
+	return iter_able
+
+def getQuestionOfType(qType):
+	iter_able = getIterableOfType(qType)
+	for num in iter_able:
+		print num, descriptions[num][0]
+	print "\nThere are a total of {} {} problems".format(len(iter_able), qType)
+
+def getRandomQuestionOfType(qType):
+	iter_able = getIterableOfType(qType)
+	randnum = random.randint(0, len(iter_able)-1)
+	print iter_able[randnum], descriptions[iter_able[randnum]]
+
+
+
+if __name__ == "__main__":
+	pre_process_questions()
+	getRandomQuestionOfType("Easy")
+
+
+
+
+
+
+
+
+  
